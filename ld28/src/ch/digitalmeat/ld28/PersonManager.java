92dfc688329703440796b6ff5c77ef36c44ca404
atombrot@gmail.com <atombrot@gmail.com>
2013-12-14 17:34:15 +0100

    added person manager

--- ld28/src/ch/digitalmeat/ld28/PersonManager.java
+++ ld28/src/ch/digitalmeat/ld28/PersonManager.java
@@ -5,8 +5,9 @@ import ch.digitalmeat.ld28.person.Person.LookingDirection;
 import ch.digitalmeat.ld28.person.Person.PersonState;
 
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.utils.Array;
 
 
@@ -21,14 +22,45 @@ public class PersonManager {
 	}
 
 	private void handlePerson(float delta, MapLayer layer, Person p) {
-		if(p.getState() == PersonState.Walking){
-			System.out.println("Update walk");
+		if(p.getState() == PersonState.Walking && p.getDirection() != LookingDirection.None){
 			float x = p.getX();
 			float moveBy = delta * (p.running ? 128f : 64f);
 			if(p.getDirection() == LookingDirection.Left){
 				moveBy *= -1;
 			}
-			p.setX(x + moveBy);
+			float newX = x + moveBy;
+			TiledMapTileLayer tl = null;
+			if(layer != null){
+				int px = tp(p.getX());
+				int py = tp(p.getY());
+				float offset = p.getDirection() == LookingDirection.Right ? + 5 : - 5;
+				int pnx = tp(newX + offset);
+
+				if(pnx != px){
+					System.out.println("Moving out from X: " + px + " ; Y: " + py );
+					tl = (TiledMapTileLayer) layer;
+					Cell cell = tl.getCell(px, py);
+					if(cell != null){
+						System.out.println("Found cell");
+						String collision = cell.getTile().getProperties().get("collision", String.class);
+						if(collision != null){
+							if(p.getDirection() == LookingDirection.Left && "left".equals(collision)){
+								newX = x;
+							}
+							else if(p.getDirection() == LookingDirection.Right && "right".equals(collision)){
+								newX = x;
+							}
+						}
+					}
+				}
+				
+				
+			}
+			p.setX(newX);
 		}
+	}
+	
+	private int tp(float val){
+		return (int)((val + 8) / 32f);
 	}
 }
